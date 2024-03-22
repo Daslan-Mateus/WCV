d2d23b480458a8986ac04c96dc8114ba79bf561b
Daslan Mateus <daslan@weg.net>
2024-03-22 12:06:21 -0300

    commitando para fazer pull

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -10,6 +10,11 @@ type Props = {
 function Formulario(props: Props) {
   const { dados } = props
 
+  function handleClick(e: any) {
+    e.preventDefault();
+      console.log('O aúdio foi gravado');
+}
+
   const handleSubmit = (event: any) => {
     event.preventDefault();
     // Aqui você pode enviar os dados para onde precisar, como uma API por exemplo
@@ -23,13 +28,13 @@ function Formulario(props: Props) {
       <form onSubmit={handleSubmit} className='formulario'>
         <div className='direita'>
           <label>Mercado:</label>
-          <input type="text" name="mercado" value={dados.mercado} />
+          <input type="text" name="mercado" value={dados.mercado} onChange={handleClick}/>
 
           <label>Alimentação:</label>
-          <input type="text" name="alimentacao" value={dados.alimentacao} />
+          <input type="text" name="alimentacao" value={dados.alimentacao} onChange={handleClick}/>
 
           <label>Invdlucro:</label>
-          <input type="text" name="invdlucro" value={dados.involucro} />
+          <input type="text" name="invdlucro" value={dados.involucro} onChange={handleClick}/>
 
           <label>Linha:</label>
           <input type="text" name="linha" value={dados.linha} />
@@ -37,35 +42,35 @@ function Formulario(props: Props) {
 
         <div>
           <label>Material Involucro:</label>
-          <input type="text" name="materialInvdlucro" value={dados.materialInvolucro} />
+          <input type="text" name="materialInvdlucro" value={dados.materialInvolucro} onChange={handleClick}/>
 
           <label>Frequência:</label>
-          <input type="text" name="frequencia" value={dados.frequencia} />
+          <input type="text" name="frequencia" value={dados.frequencia} onChange={handleClick}/>
           <label>Detalhe 1:</label>
-          <input type="text" name="detalhe1" value={dados.detalhe1} />
+          <input type="text" name="detalhe1" value={dados.detalhe1} onChange={handleClick}/>
           <label>Detalhe 2:</label>
-          <input type="text" name="detalhe2" value={dados.detalhe2} />
+          <input type="text" name="detalhe2" value={dados.detalhe2} onChange={handleClick}/>
         </div>
 
         <div>
           <label>Tipo de Enrolamento:</label>
-          <input type="text" name="tipoEnrolamento" value={dados.tipoEnrolamento} />
+          <input type="text" name="tipoEnrolamento" value={dados.tipoEnrolamento} onChange={handleClick}/>
 
           <label>Potência:</label>
-          <input type="text" name="potencia" value={dados.potencia} />
+          <input type="text" name="potencia" value={dados.potencia} onChange={handleClick}/>
           <label>Polos:</label>
-          <input type="text" name="polos" value={dados.polos} />
+          <input type="text" name="polos" value={dados.polos} onChange={handleClick}/>
           <label>Carcaça:</label>
-          <input type="text" name="carcaça" value={dados.carcaca} />
+          <input type="text" name="carcaça" value={dados.carcaca} onChange={handleClick}/>
         </div>
 
         <div>
           <label>Norma:</label>
-          <input type="text" name="norma" value={dados.norma} />
+          <input type="text" name="norma" value={dados.norma} onChange={handleClick}/>
           <label>Tensão:</label>
-          <input type="text" name="tensao" value={dados.tensao} />
+          <input type="text" name="tensao" value={dados.tensao} onChange={handleClick}/>
           <label>Variante de linha:</label>
-          <input type="text" name="varianteLinha" value={dados.varianteLinha} />
+          <input type="text" name="varianteLinha" value={dados.varianteLinha} onChange={handleClick}/>
 
           <button type="submit">Enviar</button>
         </div>
